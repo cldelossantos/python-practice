f0bc854450ffdccc7a9da9285d2ddf8858b8cc27
Claudia De Los Santos <cldelossantos@gmail.com>
2020-06-09 20:44:40 -0400

    conditionals

---
+++ python-conditional-solutions.py
@@ -0,0 +1,36 @@
+is_summer = True
+temperature = 90
+is_raining = True
+
+
+
+# if is_summer == True:
+#     print("Where are my shorts?")
+# else:
+#     print("I guess I'll wear pants today")
+
+
+
+# if temperature < 85:
+#     print("Whoa, mucho caliente")
+# else:
+#     print("Nice, the temperature is mild outside")
+
+
+# if is_raining == True:
+#     print("Grab an umbrella")
+# else:
+#     print("Put the umbrella away!")
+
+
+
+if is_summer == True and temperature > 85 and is_raining == False:
+    print("Perfect day to catch Corona at the beach!")
+elif temperature > 85 and is_raining == True:
+    print("Stay in but blast the AC")
+elif is_summer == True and temperature < 85:
+    print("Not hot enough for the beach...I guess no Corona for me :/")
+elif is_summer == False and temperature > 85:
+    print("It's so hot it feels like summer")
+else:
+    None
